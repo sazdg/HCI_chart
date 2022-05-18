20eedce722e7eb4f14814238e8cc1cd4156c5f5a
Sara <saradegra@gmail.com>
2022-05-18 19:56:27 +0200

    chart #2 cuba

--- chart.js
+++ chart.js
@@ -1,135 +1,102 @@
-$(document).ready(function(){
-
-    const myChart = document.getElementById('myChart').getContext('2d')
-
-    //var globali del grafico
-    Chart.defaults.global.defaultFontFamily = 'Arial'
-    Chart.defaults.global.defaultFontSize = 18
-    Chart.defaults.global.defaultFontColor = '#777'
-
-    //https://thewebdev.info/2020/04/28/chart-js-creating-a-chart-with-multiple-lines/
-    //la foto con il cane 
-
-    //https://stackoverflow.com/questions/19187085/what-is-the-easiest-way-to-convert-an-excel-spreadsheet-with-tabular-data-to-jso
-    //CONVERT CSV TO JSON
 
 
-    //grafico multi axis line chart 
-    //https://stackabuse.com/guide-to-creating-charts-in-javascript-with-chartjs/ 
-    var datiMarzo = {},
-        processedData = {}
 
-    var labels = []
+//var globali del grafico
+Chart.defaults.global.defaultFontFamily = 'Arial'
+Chart.defaults.global.defaultFontSize = 18
+Chart.defaults.global.defaultFontColor = '#777'
 
-    var jsonData = $.ajax({
-        url: './marzo_chart1.json',
-        dataType: 'json',
-    }).done(function (result) {
-        console.log(result)
+//https://thewebdev.info/2020/04/28/chart-js-creating-a-chart-with-multiple-lines/
+//la foto con il cane 
 
-        datiMarzo = {
-            labels: result.Data,
-            datasets: [{
-                label: 'Contagiati',
-                data: result.Contagiati,
-                borderWidth: 1,
-                fill: false,
-                borderColor: 'green'
-            }, {
-                label: 'Decessi',
-                data: result.Decessi,
-                borderWidth: 1,
-                fill: false,
-                borderColor: 'blue'
-            }, {
-                label: 'Tamponi',
-                data: result.Tamponi,
-                borderWidth: 1,
-                fill: false,
-                borderColor: 'red'
-            }]
-        }
-    })
+//https://stackoverflow.com/questions/19187085/what-is-the-easiest-way-to-convert-an-excel-spreadsheet-with-tabular-data-to-jso
+//CONVERT CSV TO JSON
 
-    const config = {
-        type: 'line',
-        data: datiMarzo,
-        // {
-        //     labels: ['lbl1', 'lbl2', 'lbl3', 'lbl4', 'lbl5'],//giorni
-        //     datasets: [
-        //     {
-        //         label: 'Contagi',
-        //         data: [10, 20, 40, 60, 100], //data1,
-        //         borderWidth: 1,
-        //         fill: false,
-        //         borderColor: 'green',
-        //     }, {
-        //         label: 'Decessi',
-        //         data: [49, 70, 20, 10, 23], //data2,
-        //         borderWidth: 1,
-        //         fill: false,
-        //         borderColor: 'blue',
-        //     }, {
-        //         label: 'Tamponi',
-        //         data: [50, 100, 20, 40, 30], //data3,
-        //         borderWidth: 1,
-        //         fill: false,
-        //         borderColor: 'red',
-        //     }
-        //     ]
+
+//grafico multi axis line chart 
+//https://stackabuse.com/guide-to-creating-charts-in-javascript-with-chartjs/ 
+const myChart = document.getElementById('myChart').getContext('2d')
+
+var labels, data, data1, data3
+
+const config = {
+    type: 'line',
+    data: datiMarzo,
+    // {
+    //     labels: ['lbl1', 'lbl2', 'lbl3', 'lbl4', 'lbl5'],//giorni
+    //     datasets: [
+    //     {
+    //         label: 'Contagi',
+    //         data: [10, 20, 40, 60, 100], //data1,
+    //         borderWidth: 1,
+    //         fill: false,
+    //         borderColor: 'green',
+    //     }, {
+    //         label: 'Decessi',
+    //         data: [49, 70, 20, 10, 23], //data2,
+    //         borderWidth: 1,
+    //         fill: false,
+    //         borderColor: 'blue',
+    //     }, {
+    //         label: 'Tamponi',
+    //         data: [50, 100, 20, 40, 30], //data3,
+    //         borderWidth: 1,
+    //         fill: false,
+    //         borderColor: 'red',
+    //     }
+    //     ]
+    // },
+    options: {
+        // title: {
+        //     display: true,
+        //     text: 'Casi covid-19 torino',
+        //     fontSize: 12
         // },
-        options: {
-            // title: {
-            //     display: true,
-            //     text: 'Casi covid-19 torino',
-            //     fontSize: 12
-            // },
-            // legend: {
-            //     display: true,
-            //     position: top,
-            //     labels: {
-            //         fontColor: '#000'
-            //     }
-            // },
-            layout: {
-                padding: {
-                    left: 50,
-                    right: 0,
-                    bottom: 0,
-                    top: 0
-                }
-            },
-            tooltips: {
-                enabled: true
+        // legend: {
+        //     display: true,
+        //     position: top,
+        //     labels: {
+        //         fontColor: '#000'
+        //     }
+        // },
+        layout: {
+            padding: {
+                left: 50,
+                right: 0,
+                bottom: 0,
+                top: 0
             }
+        },
+        tooltips: {
+            enabled: true
         }
     }
+}
 
-    var chart = new Chart(myChart, config)
+var chart = new Chart(myChart, config)
 
-    function loadMarzo() {
-        console.log("loaded Marzo")
-    }
+function loadMarzo() {
+    console.log("loaded Marzo")
+}
 
-    //default
-    function loadAprile() {
-        console.log("loaded Aprile")
-    }
+//default
+function loadAprile() {
+    console.log("loaded Aprile")
+}
 
-    //https://www.chartjs.org/docs/latest/developers/updates.html 
-    function addData(chart, label, data) {
-        chart.data.labels.push(label)
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.push(data)
-        })
-        chart.update()
-    }
+//https://www.chartjs.org/docs/latest/developers/updates.html 
+function addData(chart, label, data) {
+    chart.data.labels.push(label)
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data)
+    })
+    chart.update()
+}
 
-    function removeData(chart) {
-        chart.data.labels.pop()
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.pop()
-        })
-        chart.update()
-    }
-})
+function removeData(chart) {
+    chart.data.labels.pop()
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop()
+    })
+    chart.update()
+}
